fec44d09f33a95d09fc5737d40c305d03c64b527
Annie VanHarmelen <annalies.vanharmelen@gmail.com>
2022-06-06 15:58:48 -0300

    upload

---
+++ Books_FinalProject/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using Books_FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Books_FinalProject.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            BooksEntities context = new BooksEntities();
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # Books_FinalProject
+=======
+# Books_FinalProject
+>>>>>>> 4048cd4430c3b6eea92178cc2a09149b6e386009
